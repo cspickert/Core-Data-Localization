3191049e68dd690c179784605a291de80b1aefcb
Cameron Spickert <cspicker@gmail.com>
2011-04-11 17:43:36 -0400

    added comments to GenModelStrings.py

--- GenModelStrings.py
+++ GenModelStrings.py
@@ -1,14 +1,16 @@
+#
+# A short script to generate a xxModel.strings for a given xx.xcdatamodel file.
+#
 from objc import YES, NO, NULL
 from Foundation import *
 from CoreData import *
 from os import popen as system
 
-XCODE_DIR = '/Xcode4'
-MOMC = XCODE_DIR + '/usr/bin/momc'
+MOMC = '/Xcode4/usr/bin/momc'
 
-model_dir = '/path/to/Model.xcdatamodel'
+model_in = '/path/to/Model.xcdatamodel'
 model_out = '/tmp/model.mom'
-system('%s %s %s' % (MOMC, model_dir, model_out))
+system('%s %s %s' % (MOMC, model_i, model_out))
 
 model = NSManagedObjectModel.alloc().initWithContentsOfURL_(NSURL.fileURLWithPath_(model_out))
 alphaSort = [NSSortDescriptor.sortDescriptorWithKey_ascending_('name', YES)]
