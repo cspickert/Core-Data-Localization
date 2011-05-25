41bd88b8d700da538126034771d3d7d0df919b44
Cameron Spickert <cspicker@gmail.com>
2011-05-25 18:23:29 -0400

    added command line options to GenModelStrings.py

--- GenModelStrings.py
+++ GenModelStrings.py
@@ -3,19 +3,50 @@
 #
 from objc import YES, NO, NULL
 from Foundation import *
-from CoreData import *
-from os import popen as system
+from CoreData import NSManagedObjectModel
+from os import popen as system, rmdir, remove
+from optparse import OptionParser
+from tempfile import mkdtemp
+import sys
 
-MOMC = '/Xcode4/usr/bin/momc'
+MOMC_PATH = '/Developer/usr/bin/momc'
 
-model_in = '/path/to/Model.xcdatamodel'
-model_out = '/tmp/model.mom'
-system('%s %s %s' % (MOMC, model_i, model_out))
+if __name__ == '__main__':
+	parser = OptionParser(usage='Usage: %prog [options] model', conflict_handler='resolve')
+	parser.add_option('-h', '--help', action='help', 
+		help='show this help message and exit')
+	parser.add_option('-o', '--output', dest='outfile', 
+		metavar='OUTFILE', help='write output to <file> instead of stdout')
+	parser.add_option('-m', '--momc', dest='momc', default=MOMC_PATH, 
+		metavar='MOMC', help='path to the momc binary (defaults to /Developer/usr/bin/momc)')
+	
+	(options, args) = parser.parse_args()
+	
+	if len(args) < 1:
+		parser.error('you must provide a .xcdatamodel file')
+	
+	if options.outfile is not None:
+		outfile = open(options.outfile, 'w')
+	else:
+		outfile = sys.stdout
+	
+	tmp = mkdtemp()
+	mom = tmp + '/model.mom'
+	print mom
+	system('%s %s %s' % (options.momc, args[0], mom))
 
-model = NSManagedObjectModel.alloc().initWithContentsOfURL_(NSURL.fileURLWithPath_(model_out))
-alphaSort = [NSSortDescriptor.sortDescriptorWithKey_ascending_('name', YES)]
-for anEntity in model.entities().sortedArrayUsingDescriptors_(alphaSort):
-	print '"Entity/%s" = "%s";' % (anEntity.name(), anEntity.name())
-	for aProperty in anEntity.properties().sortedArrayUsingDescriptors_(alphaSort):
-		print '"Property/%s/Entity/%s" = "%s";' % (aProperty.name(), anEntity.name(), aProperty.name())
-	print
+	model = NSManagedObjectModel.alloc().initWithContentsOfURL_(NSURL.fileURLWithPath_(mom))
+	remove(mom)
+	rmdir(tmp)
+	
+	descriptors = [NSSortDescriptor.sortDescriptorWithKey_ascending_('name', YES)]
+	entities = model.entities().sortedArrayUsingDescriptors_(descriptors)
+	
+	for anEntity in entities:
+		entityName = anEntity.name()
+		outfile.write('"Entity/%s" = "%s";\n' % (entityName, entityName))
+		properties = anEntity.properties().sortedArrayUsingDescriptors_(descriptors)
+		for aProperty in properties:
+			propertyName = aProperty.name()
+			outfile.write('"Property/%s/Entity/%s" = "%s";\n' % (propertyName, entityName, propertyName))
+		outfile.write('\n')
